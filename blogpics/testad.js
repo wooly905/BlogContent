accd548bbf245e774ab1b44121ffad2746e61122
Yih-Cheng Lee <bruceyclee@gmail.com>
2017-11-15 22:50:30 -0800

    test ad js

---
+++ blogpics/testad.js
@@ -0,0 +1,10 @@
+var hatenadfp = hatenadfp || {};
+  hatenadfp.adUnits = hatenadfp.adUnits || [];
+  hatenadfp.adUnits.push(
+    
+      
+      
+      { unitName: 'blog_user', size: [[300, 250], [336, 280], [468, 60], ["fluid"]], divId: 'google_afc_user_container_1', allowContentMatch: true }
+    
+  );
+  hatenadfp.isNGContent = false;

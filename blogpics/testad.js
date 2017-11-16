998e8ffb2aac872ac913de0d3ba5f24c08f3d1dc
Yih-Cheng Lee <bruceyclee@gmail.com>
2017-11-15 23:04:51 -0800

    change test ad js

--- blogpics/testad.js
+++ blogpics/testad.js
@@ -1,10 +1,1 @@
-var hatenadfp = hatenadfp || {};
-  hatenadfp.adUnits = hatenadfp.adUnits || [];
-  hatenadfp.adUnits.push(
-    
-      
-      
-      { unitName: 'blog_user', size: [[300, 250], [336, 280], [468, 60], ["fluid"]], divId: 'google_afc_user_container_1', allowContentMatch: true }
-    
-  );
-  hatenadfp.isNGContent = false;
+(adsbygoogle = window.adsbygoogle || []).push({});
